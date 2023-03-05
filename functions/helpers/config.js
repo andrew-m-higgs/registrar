84f49b5ef4673f7f3816b2651bd7b8a4edd6d84e
Andrew Higgs <andrew.m.higgs@gmail.com>
2023-03-05 22:43:42 +0200

    Setup basic file structure

---
+++ functions/helpers/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tableName: () => {
+    // Table name from Airtable setup
+    return 'DopeShit';
+  },
+ adminRoleName: () => {
+    // The admin role name used on your server for this bot    
+    return 'Creator';
+  },
+};
