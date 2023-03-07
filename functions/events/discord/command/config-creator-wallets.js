447f1ec5ea412c7147e3e4f0601a20b4fd2dfa74
Andrew Higgs <andrew.m.higgs@gmail.com>
2023-03-07 08:33:47 +0200

    Move 'let isAdmin = -1;' below the comment about needing a function as it will not be needed when this function is sorted.
    Signed-off-by: Andrew Higgs <andrew.m.higgs@gmail.com>

--- functions/events/discord/command/config-creator-wallets.js
+++ functions/events/discord/command/config-creator-wallets.js
@@ -5,3 +5,8 @@ console.log('###################################################################
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 const config    = require('../../../../helpers/config.js');
 const functions = require('../../../../helpers/functions.js');
+
+let walletStrings = context.params.event.data.options;
+let isAdmin = functions.isAdmin(context.params.event.member.user.id);
+let userRoles = context.params.event.member.roles;
+console.log("config: " + userRoles);
