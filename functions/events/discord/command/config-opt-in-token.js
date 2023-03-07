629925fe35c9f4b25287717beaf2c8586ae2c8cf
Andrew Higgs <andrew.m.higgs@gmail.com>
2023-03-07 08:34:41 +0200

    Add the command for setting the project's opt-in token.

--- functions/events/discord/command/config-opt-in-token.js
+++ functions/events/discord/command/config-opt-in-token.js
@@ -5,3 +5,42 @@ console.log('###################################################################
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 const config    = require('../../../../helpers/config.js');
 const functions = require('../../../../helpers/functions.js');
+
+let optInToken = context.params.event.data.options[0].value;
+
+//Need a function to determine admin status of member
+// functions.isAdmin()
+let adminID    = -1
+let guild_id   = context.params.event.guild_id;
+let user_roles = context.params.event.member.roles;
+
+//get list of server roles
+let roles = await lib.discord.guilds['@0.2.4'].roles.list({
+  guild_id: guild_id,
+});
+
+//get the ID of the adminRoleName
+for (let i = 0; i < roles.length; i++){
+  if (roles[i].name == config.adminRoleName()){
+    adminID = roles[i].id;
+  }
+}
+
+//add the project_name if member has admin role
+if (user_roles.includes(adminID)) {
+  await lib.utils.kv['@0.1.16'].set({
+    key: "ProjectName-" + guild_id,
+    value: projectName
+  });
+
+  //let the member know it happened
+  let followUp = await lib.discord.interactions['@1.0.1'].responses.ephemeral.create({
+    token: `${context.params.event.token}`,
+    content: 'The project name was set to **' + projectName + '**. Run the command again to change it. :-)'
+  })
+} else {
+  let followUp = await lib.discord.interactions['@1.0.1'].responses.ephemeral.create({
+    token: `${context.params.event.token}`,
+    content: 'You do not have permission to set the project name to **' + projectName + '**. Run the command again after being given the correct role to set it. :-)'
+  })
+}
