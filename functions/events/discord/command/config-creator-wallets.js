b499607cdff5447d8b2a09d1d8ddfaad14fd213c
Andrew Higgs <andrew.m.higgs@gmail.com>
2023-03-08 09:48:23 +0200

    Add config-creator-wallets command.

--- functions/events/discord/command/config-creator-wallets.js
+++ functions/events/discord/command/config-creator-wallets.js
@@ -6,7 +6,25 @@ const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 const config    = require('../../../../helpers/config.js');
 const functions = require('../../../../helpers/functions.js');
 
-let walletStrings = context.params.event.data.options;
-let isAdmin = functions.isAdmin(context.params.event.member.user.id);
-let userRoles = context.params.event.member.roles;
-console.log("config: " + userRoles);
+let walletStrings = context.params.event.data.options[0].value;
+let guild_id = context.params.event.guild_id;
+let isAdmin = functions.isAdmin(context);
+let message_content = "";
+
+if (isAdmin) {
+  // IS AN ADMIN
+  await lib.utils.kv['@0.1.16'].set({
+    key: "CreatorWallets: " + guild_id,
+    value: walletStrings
+  }).catch(console.error(e));
+
+  message_content = "The creator wallet(s) were set to (**" + walletStrings + "**). Re-run the command to change it/them."
+} else {
+  // NOT AN ADMIN
+  message_content = "You do not have permission to run this command. Run the command again after being given the correct role. :-)"
+}
+
+let message = await lib.discord.interactions['@1.0.1'].responses.ephemeral.create({
+    token: context.params.event.token,
+    content: message_content
+})
