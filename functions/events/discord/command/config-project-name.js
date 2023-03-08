818d4129dbb894d5dbe98dcb52a974f9a7022093
Andrew Higgs <andrew.m.higgs@gmail.com>
2023-03-08 09:54:43 +0200

    Add config-project-name.js command.

--- functions/events/discord/command/config-project-name.js
+++ functions/events/discord/command/config-project-name.js
@@ -6,40 +6,25 @@ const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 const config    = require('../../../../helpers/config.js');
 const functions = require('../../../../helpers/functions.js');
 
-let projectName = context.params.event.data.options[0].value;
+let projectName     = context.params.event.data.options[0].value;
+let guild_id        = context.params.event.guild_id;
+let isAdmin         = functions.isAdmin(context);
+let message_content = "";
 
-//Need a function to determine admin status of member
-// functions.isAdmin()
-let guild_id   = context.params.event.guild_id;
-let user_roles = context.params.event.member.roles;
-
-//get list of server roles
-let roles = await lib.discord.guilds['@0.2.4'].roles.list({
-  guild_id: guild_id,
-});
-
-//get the ID of the adminRoleName
-for (let i = 0; i < roles.length; i++){
-  if (roles[i].name == config.adminRoleName()){
-    let adminID = roles[i].id;
-  }
-}
-
-//add the project_name if member has admin role
-if (user_roles.includes(adminID)) {
+if (isAdmin) {
+  // IS AN ADMIN
   await lib.utils.kv['@0.1.16'].set({
     key: "ProjectName-" + guild_id,
     value: projectName
-  });
+  }).catch(console.error(e));
 
-  //let the member know it happened
-  let followUp = await lib.discord.interactions['@1.0.1'].responses.ephemeral.create({
-    token: `${context.params.event.token}`,
-    content: 'The project name was set to **' + projectName + '**. Run the command again to change it. :-)'
-  })
+  message_content = "The project name was set to **" + projectName + "**. Re-run the command to change it."
 } else {
-  let followUp = await lib.discord.interactions['@1.0.1'].responses.ephemeral.create({
-    token: `${context.params.event.token}`,
-    content: 'You do not have permission to set the project name to **' + projectName + '**. Run the command again after being given the correct role to set it. :-)'
-  })
+  // NOT AN ADMIN
+  message_content = "You do not have permission to run this command. Run the command again after being given the correct role. :-)"
 }
+
+let message = await lib.discord.interactions['@1.0.1'].responses.ephemeral.create({
+    token: context.params.event.token,
+    content: message_content
+})
