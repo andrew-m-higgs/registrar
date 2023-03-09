8f422f6d28538aaa58a4e694f559dd2fbc11ccf7
Andrew Higgs <andrew.m.higgs@gmail.com>
2023-03-09 08:45:14 +0200

    Add config-registered-role.js command

--- functions/events/discord/command/config-registered-role.js
+++ functions/events/discord/command/config-registered-role.js
@@ -5,3 +5,27 @@ console.log('###################################################################
 const lib       = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 const config    = require('../../../../helpers/config.js');
 const functions = require('../../../../helpers/functions.js');
+
+let regRole         = context.params.event.data.options[0].value;
+let guild_id        = context.params.event.guild_id;
+let isAdmin         = functions.isAdmin(context);
+let message_content = "";
+
+if (isAdmin) {
+  // IS AN ADMIN
+  await lib.utils.kv['@0.1.16'].set({
+    key: "RegisteredRole-" + guild_id,
+    value: regRole
+  }).catch(console.error(e));
+
+  message_content = "The registered role was set to **" + regRole + "**. Re-run the command to change it."
+} else {
+  // NOT AN ADMIN
+  message_content = "You do not have permission to run this command. Run the command again after being given the correct role. :-)"
+}
+
+let message = await lib.discord.interactions['@1.0.1'].responses.ephemeral.create({
+    token: context.params.event.token,
+    content: message_content
+})
+
