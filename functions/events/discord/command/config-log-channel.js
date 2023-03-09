60995c13e8bd0236ef47f80ec4e5d8757ce47b94
Andrew Higgs <andrew.m.higgs@gmail.com>
2023-03-09 08:39:07 +0200

    Just a little code neatening

--- functions/events/discord/command/config-log-channel.js
+++ functions/events/discord/command/config-log-channel.js
@@ -2,7 +2,7 @@ console.log('###################################################################
 console.log('        command/config-log-channel.js STARTING...')
 console.log('################################################################################')
 
-const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+const lib       = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 const config    = require('../../../../helpers/config.js');
 const functions = require('../../../../helpers/functions.js');
 
