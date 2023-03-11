5a611511707514a7dcd7039d57b7ca1d0aa70814
Andrew Higgs <andrew.m.higgs@gmail.com>
2023-03-11 11:43:42 +0200

    Add basic colours and emojis to config to make sure the same are used everywhere
    Signed-off-by: Andrew Higgs <andrew.m.higgs@gmail.com>

--- functions/helpers/config.js
+++ functions/helpers/config.js
@@ -3,8 +3,24 @@ module.exports = {
     // Table name from Airtable setup
     return 'TableName';
   },
- adminRoleName: () => {
+  adminRoleName: () => {
     // The admin role name used on your server for this bot    
     return 'Creator';
   },
+
+  // Some colours to use
+  green: () => {
+    return 0x78B159;
+  },
+  red: () => {
+    return 0xDD2E44;
+  },
+
+  // Some emojis to use
+  successMark: () => {
+    return ":heavy_check_mark:"
+  },    
+  failMark: () => {
+    return ":heavy_multiplication_x:"
+  },    
 };
